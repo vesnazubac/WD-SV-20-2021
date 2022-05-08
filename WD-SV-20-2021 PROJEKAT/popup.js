29404a43829f1715edf09baaa96c723ffcdc342d
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2022-05-08 12:22:21 +0200

    Add files via upload
    KONACNA VERZIA PROJEKTA

---
+++ WD-SV-20-2021 PROJEKAT/popup.js
@@ -0,0 +1,22 @@
+
+
+
+/*document.getElementById('.link-detaljan-opis').addEventListener('click',function(){
+
+    document.querySelector('.pop-up').style.display='flex';
+
+
+
+})*/
+
+/*document.querySelector('.close').addEventListener('click',function(){
+
+    document.querySelector('.pop-up').style.display='none';
+
+
+
+})*/
+
+/*function myFunction(){
+
+    document.querySelector('.pop-up').style.display='flex';*/
